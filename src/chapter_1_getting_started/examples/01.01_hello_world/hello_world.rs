cb5300e52fcac74a2c27158cafedd725889d564f
whoamih4csor <122777930+whoamih4csor@users.noreply.github.com>
2023-03-08 13:30:53 -0400

    init repo

---
+++ src/chapter_1_getting_started/examples/01.01_hello_world/hello_world.rs
@@ -0,0 +1,15 @@
+/*
+AUTHOR: whoamih4csor
+DATE: Wednesday, March 8, 2023
+SECTION: 1.1 Getting Started
+
+This Rust program is a simple example that prints the words "Hello World".
+The only way to learn a new programming language is by writing programs in it.
+
+to compile:
+cargo run --bin example_1_1
+*/
+
+fn main() {  //this is the entry point
+    println!("Hello, world!"); //this is a macro that prints "Hello World"
+}
---
+++ src/chapter_1_getting_started/exercises/01.01/01_01.rs
@@ -0,0 +1,31 @@
+/*
+AUTHOR: whoamih4csor
+DATE: Wednesday, March 8, 2023
+
+Exercise 1-1. Run the "hello, world" program on your system.Experiment
+with leaving out parts of the program, to see what error messages you get. 
+
+to compile:
+cargo run --bin exercise_1_1
+*/
+
+fn main() {
+    /*
+    This code is correct and has no errors.
+
+    println!("Hello World");
+    */
+    
+    /*
+    ERRORS:
+    If you leave parts out of your code, you will get compile-time errors.
+
+    Examples:
+
+        println!("Hello World);
+
+        println("Hello World");
+
+        println!("Hello World";
+    */
+}
